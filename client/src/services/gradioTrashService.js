4eda60297e0bf648f726f6c3865c250b385b56e0
Arthur Guillemin <aguillemin291@gmail.com>
2025-05-04 19:20:46 +0200

    Update gradio service

--- client/src/services/gradioTrashService.js
+++ client/src/services/gradioTrashService.js
@@ -5,13 +5,14 @@ const clientPromise = Client.connect("ankz22/trash-classifier");
 export async function classifyWasteImage(imageFile) {
   const client = await clientPromise;
   const response = await client.predict("/predict", { image: imageFile });
-  const data = response.data[0].data;
+  const rawData = response.data?.[0]?.data;
 
-  const classification = data.map(([objet, poubelle, confiancePourcent]) => ({
+  if (!rawData || !Array.isArray(rawData)) {
+    throw new Error("mauvais format de reponse");
+  }
+  return rawData.map(([objet, poubelle, confiancePourcent]) => ({
     objet,
     poubelle,
     confiance: confiancePourcent,
   }));
-
-  return classification;
 }
