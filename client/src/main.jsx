52e0ce07ed4dbb05812a977ea0ed454da02cf5a2
Arthur Guillemin <aguillemin291@gmail.com>
2025-05-20 15:40:27 +0200

    Fix PWA pour de bon

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,7 +2,6 @@
 import { StrictMode, useEffect } from "react";
 import { createRoot } from "react-dom/client";
 import { registerSW } from "virtual:pwa-register";
-import "./index.css";
 import App from "./App.jsx";
 
 const updateSW = registerSW({
--- client/vite.config.js
+++ client/vite.config.js
@@ -1,4 +1,6 @@
-export default defineConfig({
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';export default defineConfig({
   plugins: [
     react(),
     VitePWA({
