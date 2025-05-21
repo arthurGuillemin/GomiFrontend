1112258f3cf75349458173f40a9a1ed273f4021d
yelineeee <153738182+yelineeee@users.noreply.github.com>
2025-05-21 09:54:25 +0200

    HomePage, JeTriePage, JeCuisinePage

---
+++ client/src/pages/JeTrie.jsx
@@ -0,0 +1,37 @@
+
+import React from 'react';
+import TrashyFriends from '../assets/trashyFriends.svg';
+import '../assets/global.css';
+import { FiSearch } from 'react-icons/fi';
+
+
+const JeTrie = () => {
+  return (
+    <div>
+        <article  className='jeTrieHome'>
+             <section>
+                <div>
+                    <h1>Je trie</h1>
+                    <p>Prenez une photo de votre déchet pour savoir instantanément dans quelle poubelle il doit aller. Verre, plastique, compost ou ordures : l’appli vous guide pour trier facilement et correctement, réduire votre impact sur l’environnement et adopter les bons gestes au quotidien.</p>
+                </div>
+                <div className="searchContainer jeTrie">
+                    <input
+                        type="text"
+                        placeholder="Rechercher un déchet..."
+                        className="searchInput"
+                        />
+                    <button className="searchButton">
+                    <FiSearch size={20} />
+                    </button>
+                </div>
+            </section>
+            <section>
+                <img src={TrashyFriends}  />
+            </section>
+        </article>
+    </div>
+    
+  );
+};
+
+export default JeTrie;
