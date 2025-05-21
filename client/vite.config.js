0a7fa043eaffe8689a74e7cfd196ff29ba3a11bb
Arthur Guillemin <aguillemin291@gmail.com>
2025-05-21 11:08:01 +0200

    Fix pwa in vite.config

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,13 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: "autoUpdate",
-      includeAssets: ["favicon.svg", "apple-touch-icon.png", "masked-icon.svg"],
+      includeAssets: [
+        "favicon.svg",
+        "apple-touch-icon.png",
+        "masked-icon.svg",
+        "icon192.png",
+        "icon512.png"
+      ],
       manifest: {
         name: "Gomi",
         short_name: "Gomi",
@@ -18,12 +24,12 @@ export default defineConfig({
         start_url: "/",
         icons: [
           {
-            src: "/icon-192.png",
+            src: "/icon192.png",
             sizes: "192x192",
             type: "image/png",
           },
           {
-            src: "/icon-512.png",
+            src: "/icon512.png",
             sizes: "512x512",
             type: "image/png",
           },
