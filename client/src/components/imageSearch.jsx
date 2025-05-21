12d2b948f4ea5f20225b9110d1c1e641299770c6
yelineeee <153738182+yelineeee@users.noreply.github.com>
2025-05-21 13:16:34 +0200

    composant import photo

---
+++ client/src/components/imageSearch.jsx
@@ -0,0 +1,53 @@
+import { FiSearch } from "react-icons/fi";
+import { FaArrowRight } from "react-icons/fa";
+import { useState } from "react";
+
+export default function ImageSearch({ onImageSelected, onSend }) {
+    const [selectedImage, setSelectedImage] = useState(null);
+    const [file, setFile] = useState(null);
+    const [fileName, setFileName] = useState("");
+
+    const handleImageChange = (event) => {
+        const selectedFile = event.target.files[0];
+        if (selectedFile) {
+            const imageUrl = URL.createObjectURL(selectedFile);
+            setSelectedImage(imageUrl);
+            setFile(selectedFile);
+            setFileName(selectedFile.name);
+            if (onImageSelected) {
+                onImageSelected(selectedFile);
+            }
+        }
+    };
+
+    const handleSend = () => {
+        if (file && onSend) {
+            onSend(file);
+        }
+    };
+
+    return (
+        <div className="searchContainer">
+            <div className="inputWrapper">
+                <label className="fileInputLabel">
+                    <FiSearch className="searchIcon" />
+                    <input
+                        type="file"
+                        accept="image/*"
+                        onChange={handleImageChange}
+                        className="fileInput"
+                    />
+                    {!fileName && (
+                        <span className="placeholderText">Importer votre photo</span>
+                    )}
+                    {fileName && (
+                        <span className="fileName">{fileName}</span>
+                    )}
+                </label>
+                <button className="sendButton" onClick={handleSend}>
+                    <FaArrowRight color="white" />
+                </button>
+            </div>
+        </div>
+    );
+}
