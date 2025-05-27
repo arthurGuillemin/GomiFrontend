4bf67d939636b7e862310ff744943e17e2a92009
Arthur Guillemin <aguillemin291@gmail.com>
2025-05-27 14:56:16 +0200

    💻 feat(auth): add validation for signup data 💻

---
+++ client/src/utils/dataValidation.js
@@ -0,0 +1,30 @@
+import zxcvbn from 'zxcvbn';
+
+export const validateSignupInput = ({ email, password }) => {
+  const isValidEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+  if (!isValidEmail) {
+    return {
+      isValid: false,
+      message: "Email invalide.",
+    };
+  }
+  try {
+    const result = zxcvbn(password);
+    const score = result.score;
+    const reason = result.feedback.warning || "Mot de passe correct";
+
+    if (score < 3) {
+      return {
+        isValid: false,
+        message: `Mot de passe trop faible : ${reason}`,
+      };
+    }
+    return { isValid: true };
+  } catch (error) {
+    console.error(`Erreur pdt la vérification du mdp : ${error}`);
+    return {
+      isValid: false,
+      message: "Erreur dans la validation du mot de passe.",
+    };
+  }
+};
